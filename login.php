f5ca86d75a33a975d6f9ae61472fbc1a64a55b85
onionion <87262365+987XIA@users.noreply.github.com>
2024-05-21 23:36:44 -0400

    Add login and signup pages
    Created a new signup page with form validation for username and password confirmation.
    Added a login page for user authentication.
    Included JavaScript function to reset the signup form to its initial state.
    Implemented PHP backend for handling user registration and verifying unique usernames.

---
+++ login.php
@@ -0,0 +1,80 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="box form-box">
+
+            <?php
+            include("php/config.php");
+            if (isset($_POST['submit'])) {
+                $username = mysqli_real_escape_string($con, $_POST['username']);
+                $password = mysqli_real_escape_string($con, $_POST['password']);
+
+                //verify the username and password matched
+
+                $result = mysqli_query($con, "SELECT * FROM users WHERE Username='$username' AND Password='$password'") or die("Select Error");
+                $row = mysqli_fetch_assoc($result);
+
+                if (is_array($row) && !empty($row)) {
+                    $_SESSION['valid'] = $row['Username'];
+                    $_SESSION['username'] = $row['Username'];
+                    $_SESSION['password'] = $row['Password'];
+                    $_SESSION['id'] = $row['Id'];
+
+                    // if login successful, then goes to dashboard page
+                    header("Location: dashboard.php");
+                    exit;
+                    // ensure stops after redirection
+
+                } else {
+                    echo "<div class='message'>
+                            <p>Wrong Username/Password</p>
+                            </div> <br>";
+                    echo "<a href='login.php'><button class='btn'>Login</button>";
+                }
+            } else {
+
+            ?>
+
+                <div class="header">
+                    <header>Login</header>
+                    <p>Please fill in your credentials to login.</p>
+                </div>
+                <form action="" method="post">
+                    <div class="field input">
+                        <label for="username">Username:</label><br>
+                        <input type="text" name="username" id="username" autocomplete="off" required><br>
+                    </div>
+                    <div class="field input">
+                        <label for="password">Password:</label><br>
+                        <input type="password" name="password" id="password" autocomplete="off" required><br>
+                    </div>
+                    <div class="field">
+                        <input type="submit" class="btn" name="submit" value="Login">
+                    </div>
+                    <div class="links">
+                        Don't have an account?
+                        <a href="signup.php">Sign up now.</a>
+                    </div>
+
+                </form>
+
+        </div>
+    <?php } ?>
+    </div>
+
+</body>
+
+</html>
---
+++ signup.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="js/script.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="box form-box">
+            <?php
+            include("php/config.php");
+            if (isset($_POST['submit'])) {
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+                $cpassword = $_POST['cpassword'];
+
+                //check if pwd matches cpwd
+                if ($password != $cpassword) {
+                    echo "<div class='message'>
+                         <p>Passwords do not match. Please try again.</p>
+                         </div> <br>";
+                    echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
+                } else {
+                    //verify the unique username
+                    $verify_query = mysqli_query($con, "SELECT Username FROM users WHERE Username='$username'");
+                    if (mysqli_num_rows($verify_query) != 0) {
+                        echo "<div class='message'>
+                            <p>Oops! Somebody have used this name.</p>
+                        </div> <br>";
+                        echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
+                    } else {
+                        mysqli_query($con, "INSERT INTO users(Username,Password) VALUES('$username','$password')") or die("Error");
+
+                        echo "<div class='message'>
+                                <p>Registration Successful</p>
+                                </div> <br>";
+                        echo "<a href='login.php'><button class='btn'>Login</button>";
+                    }
+                }
+            } else {
+
+            ?>
+
+
+                <div class="header">
+                    <header>Sign Up</header>
+                    <p>Please fill this form to create an account.</p>
+                </div>
+                <form action="" method="post" id="signupForm">
+                    <div class="field input">
+                        <label for="username">Username:</label><br>
+                        <input type="text" name="username" id="username" autocomplete="off" required><br>
+                    </div>
+                    <div class="field input">
+                        <label for="password">Password:</label><br>
+                        <input type="password" name="password" id="password" autocomplete="off" required><br>
+                    </div>
+                    <div class="field input">
+                        <label for="password">Confirm Password:</label><br>
+                        <input type="password" name="cpassword" id="cpassword" autocomplete="off" required><br>
+                    </div>
+                    <div class="field">
+                        <input type="submit" class="btn" name="submit" value="Submit">
+                    </div>
+                    <div class="field">
+                        <input type="submit" id="reset" class="btn" name="reset" value="Reset" onclick="resetForm()">
+                    </div>
+                    <div class="links">
+                        Already have an account?
+                        <a href="login.php">Login here.</a>
+                    </div>
+
+                </form>
+
+        </div>
+    <?php } ?>
+    </div>
+
+</body>
+
+</html>
